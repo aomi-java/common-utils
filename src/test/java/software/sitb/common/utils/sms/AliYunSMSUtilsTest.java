011b1c3038bad17fcd0e598828bea82c8e6056fb
Sean <sean.snow@live.com>
2018-08-13 14:24:25 +0800

    test

---
+++ src/test/java/software/sitb/common/utils/sms/AliYunSMSUtilsTest.java
@@ -0,0 +1,23 @@
+package software.sitb.common.utils.sms;
+
+import org.junit.Test;
+
+/**
+ * @author 田尘殇Sean(sean.snow @ live.com) createAt 2018/8/13
+ */
+public class AliYunSMSUtilsTest {
+
+
+    @Test
+    public void sendSms(){
+        AliYunSMSUtils.sendSms(
+                "aaa",
+                "bbb",
+                "hello",
+                "111",
+                "",
+                "13000000000"
+        );
+    }
+
+}
